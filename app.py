f8e130fa6ec2aad21aa6959554fb14f072254b17
Ethan Dunne <richardethandunne99@gmail.com>
2022-12-14 22:12:55 +0000

    updating fields to reflect Champion

--- app.py
+++ app.py
@@ -45,14 +45,14 @@ app.config['SECRET_KEY'] = 'b00785513'
 
 #establishing connections between webapp and mongodb
 client = MongoClient("mongodb://127.0.0.1:27017")
-db = client.bizDB
-businesses = db.biz
+db = client.champDB
+champions = db.champions
 users = db.users
 blacklist = db.blacklist
 
 #creating pagination for data within dataset
-@app.route("/api/v1.0/businesses", methods=["GET"])
-def show_all_businesses():
+@app.route("/api/v1.0/champions", methods=["GET"])
+def show_all_champions():
     page_num, page_size = 1, 10
     if request.args.get("pn"):
         page_num = int(request.args.get('pn'))
@@ -63,94 +63,94 @@ def show_all_businesses():
     page_start = (page_size * (page_num - 1))
     
     data_to_return = []
-    for business in businesses.find().skip(page_start).limit(page_size):
-        business["_id"] = str(business["_id"])
-        for review in business["reviews"]:
+    for champion in champions.find().skip(page_start).limit(page_size):
+        champion["_id"] = str(champion["_id"])
+        for review in champion["reviews"]:
             review["_id"] = str(review["_id"])
-        data_to_return.append(business)
+        data_to_return.append(champion)
         
     return make_response( jsonify(data_to_return), 200)
 
 #creating GET endpoint for our REST api
-@app.route("/api/v1.0/businesses/<string:id>", methods = ["GET"])
-def show_one_business(id):
-    #validating whetehr business ID is valid
+@app.route("/api/v1.0/champions/<string:id>", methods = ["GET"])
+def show_one_champion(id):
+    #validating whetehr champion ID is valid
     if len(id) != 24 or not all(c in string.hexdigits for c in id):
-        return make_response(jsonify({"error" : "Invalid Business ID"}), 404)
-    business = businesses.find_one({"_id": ObjectId(id)}) #finding one business within the DB using mongodb commands
-    if business is not None: 
-        business["_id"] = str(business["_id"]) 
-        for review in business["reviews"]: 
+        return make_response(jsonify({"error" : "Invalid champion ID"}), 404)
+    champion = champions.find_one({"_id": ObjectId(id)}) #finding one champion within the DB using mongodb commands
+    if champion is not None: 
+        champion["_id"] = str(champion["_id"]) 
+        for review in champion["reviews"]: 
             review["_id"] = str(review["_id"]) 
-        return make_response(jsonify( [business] ), 200) 
+        return make_response(jsonify( [champion] ), 200) 
     else:
-        return make_response(jsonify({"error" : "Invalid Business ID"}), 404) #bad response
+        return make_response(jsonify({"error" : "Invalid champion ID"}), 404) #bad response
 
-#creating POST endpoint for our REST api
-@app.route("/api/v1.0/businesses/", methods = ["POST"])
-@jwt_required
-def add_new_business():
-    if len(id) != 24 or not all(c in string.hexdigits for c in id):
-        return make_response(jsonify({"error" : "Invalid Business ID"}), 404)
+# #creating POST endpoint for our REST api
+# @app.route("/api/v1.0/champions/", methods = ["POST"])
+# @jwt_required
+# def add_new_champion():
+#     if len(id) != 24 or not all(c in string.hexdigits for c in id):
+#         return make_response(jsonify({"error" : "Invalid champion ID"}), 404)
     
-    if "name" in request.form and "town"  in request.form and "rating" in request.form: #estalbishing data fields
-        #new_business creates schema for adding a new business object
-        new_business = {
-            "name": request.form["name"],
-            "town": request.form["town"],
-            "rating": request.form["rating"],
-            "reviews": []
-        }
-        #DB command to add one new business object
-        new_business_id = businesses.insert_one(new_business)
-        new_business_link = "http://127.0.0.1:5000/api/v1.0/businesses/" + \
-            str(new_business_id.inserted_id)
-        return make_response( jsonify({"url" : new_business_link}), 201)
-    else:
-        return make_response(jsonify({"error" : "Missing form data"}), 404)
+#     if "name" in request.form and "town"  in request.form and "rating" in request.form: #estalbishing data fields
+#         #new_champion creates schema for adding a new champion object
+#         new_champion = {
+#             "name": request.form["name"],
+#             "town": request.form["town"],
+#             "rating": request.form["rating"],
+#             "reviews": []
+#         }
+#         #DB command to add one new champion object
+#         new_champion_id = champions.insert_one(new_champion)
+#         new_champion_link = "http://127.0.0.1:5000/api/v1.0/champions/" + \
+#             str(new_champion_id.inserted_id)
+#         return make_response( jsonify({"url" : new_champion_link}), 201)
+#     else:
+#         return make_response(jsonify({"error" : "Missing form data"}), 404)
     
-@app.route("/api/v1.0/businesses/<string:id>", methods = ["PUT"])
-@jwt_required
-def edit_business(id):
-    if len(id) != 24 or not all(c in string.hexdigits for c in id):
-        return make_response(jsonify({"error" : "Invalid Business ID"}), 404)
+# @app.route("/api/v1.0/champions/<string:id>", methods = ["PUT"])
+# @jwt_required
+# def edit_champion(id):
+#     if len(id) != 24 or not all(c in string.hexdigits for c in id):
+#         return make_response(jsonify({"error" : "Invalid champion ID"}), 404)
     
-    if "name" in request.form and "town"  in request.form and "rating" in request.form:
-        result = businesses.update_one(
-            #almost the same as adding a new business except business ID is required in order to update form data
-            {"_id": ObjectId(id) }, 
-            {
-                "$set" : {
-                    "name" : request.form["name"],
-                    "town": request.form["town"],
-                    "rating": request.form["rating"],
-                }
-            }
-        )
-        #if the business ID was correct
-        if result.matched_count ==1:
-            edit_business_link = "http://127.0.0.1:5000/api/v1.0/businesses/" + id
-            return make_response( jsonify({ "url": edit_business_link }), 200)
+#     if "name" in request.form and "town"  in request.form and "rating" in request.form:
+#         result = champions.update_one(
+#             #almost the same as adding a new champion except champion ID is required in order to update form data
+#             {"_id": ObjectId(id) }, 
+#             {
+#                 "$set" : {
+#                     "name" : request.form["name"],
+#                     "town": request.form["town"],
+#                     "rating": request.form["rating"],
+#                 }
+#             }
+#         )
+#         #if the champion ID was correct
+#         if result.matched_count ==1:
+#             edit_champion_link = "http://127.0.0.1:5000/api/v1.0/champions/" + id
+#             return make_response( jsonify({ "url": edit_champion_link }), 200)
     
-        else:
-            return make_response(jsonify({"error" : "Invalid business ID"}), 404)
-    else:
-        return make_response(jsonify({"error" : "Missing form data"}), 404)
+#         else:
+#             return make_response(jsonify({"error" : "Invalid champion ID"}), 404)
+#     else:
+#         return make_response(jsonify({"error" : "Missing form data"}), 404)
     
-@app.route("/api/v1.0/businesses/<string:id>", methods = ["DELETE"])
+@app.route("/api/v1.0/champions/<string:id>", methods = ["DELETE"])
 @jwt_required
 @admin_required
-def delete_business(id):
+def delete_champion(id):
     if len(id) != 24 or not all(c in string.hexdigits for c in id):
-        return make_response(jsonify({"error" : "Invalid Business ID"}), 404)
+        return make_response(jsonify({"error" : "Invalid champion ID"}), 404)
     #DB commant to delete one object using the object ID
-    result = businesses.delete_one({"_id": ObjectId(id)})
+    result = champions.delete_one({"_id": ObjectId(id)})
     if result.deleted_count == 1:
         return make_response( jsonify({}), 204)
     else:
-        return make_response(jsonify({"error" : "Invalid business ID"}), 404)
+        return make_response(jsonify({"error" : "Invalid champion ID"}), 404)
     
-@app.route("/api/v1.0/businesses/<string:id>/reviews", methods = ["POST"])
+@app.route("/api/v1.0/champions/<string:id>/reviews", methods = ["POST"])
 def add_new_review(id):
     #new_review defines schema used for review form data
     new_review = {
@@ -159,44 +159,44 @@ def add_new_review(id):
         "comment": request.form["comment"],
         "stars": request.form["stars"]
     }
-    #We need to update the buisness object with the new review so we pass the business ID and update the review field with our new review
-    businesses.update_one(
+    #We need to update the buisness object with the new review so we pass the champion ID and update the review field with our new review
+    champions.update_one(
         {"_id" : ObjectId(id)},
         {
             '$push' : {"reviews" : new_review}
         }
     )
-    new_review_link = "http://127.0.0.1:5000/api/v1.0/businesses/" + id + \
+    new_review_link = "http://127.0.0.1:5000/api/v1.0/champions/" + id + \
         "/reviews/" + str(new_review["_id"])
     return make_response(jsonify({"url": new_review_link}), 201)
 
-@app.route("/api/v1.0/businesses/<string:id>/reviews", methods = ["GET"])
+@app.route("/api/v1.0/champions/<string:id>/reviews", methods = ["GET"])
 def fetch_all_reviews(id):
     #definding our returned data (reviews) as a list
     data_to_return = []
-    business = businesses.find_one(
-        #we find one review via the business ID and then project the review without its ID
+    champion = champions.find_one(
+        #we find one review via the champion ID and then project the review without its ID
         {"_id" : ObjectId(id)}, {"reviews" : 1, "_id": 0}
     )
-    for review in business["reviews"]:
+    for review in champion["reviews"]:
         review["_id"] = str(review["_id"])
         data_to_return.append(review)
     return make_response( jsonify( data_to_return ), 200)
 
-@app.route("/api/v1.0/businesses/<string:id>/reviews/<string:review_id>", methods = ["GET"])
+@app.route("/api/v1.0/champions/<string:id>/reviews/<string:review_id>", methods = ["GET"])
 @jwt_required
 def fetch_one_review(id, review_id):
-    business = businesses.find_one(
+    champion = champions.find_one(
         { "reviews._id" : ObjectId(review_id) },
         {"_id" : 0, "reviews.$" : 1}
     )
-    if business is None:
-        return make_response(jsonify({"error" : "Invalid Business or Review ID"}), 404)
+    if champion is None:
+        return make_response(jsonify({"error" : "Invalid champion or Review ID"}), 404)
     else:
-        business["reviews"][0]["_id"] = str(business["reviews"][0]["_id"])
-        return make_response( jsonify(business["reviews"][0]), 200)
+        champion["reviews"][0]["_id"] = str(champion["reviews"][0]["_id"])
+        return make_response( jsonify(champion["reviews"][0]), 200)
     
-@app.route("/api/v1.0/businesses/<string:id>/reviews/<string:review_id>", methods = ["PUT"])
+@app.route("/api/v1.0/champions/<string:id>/reviews/<string:review_id>", methods = ["PUT"])
 @jwt_required
 def edit_review(id, review_id):
     #using $ positonal operator again as it poitns to the single review that matches the ID given, so we onyl update that one review
@@ -205,22 +205,22 @@ def edit_review(id, review_id):
         "reviews.$.comment" : request.form["comment"],
         "reviews.$.stars" : request.form["stars"]
     }
-    #We update our business object by setting the review returned by the search as the new review (edited review)
-    businesses.update_one(
+    #We update our champion object by setting the review returned by the search as the new review (edited review)
+    champions.update_one(
         {"reviews._id": ObjectId(review_id)},
         {"$set": edited_review}
     )
-    edit_review_url = "http://127.0.0.1:5000/api/v1.0/businesses/" + id + \
+    edit_review_url = "http://127.0.0.1:5000/api/v1.0/champions/" + id + \
         "/reviews/" + review_id
     return make_response( jsonify({"url" : edit_review_url}), 200)
 
-@app.route("/api/v1.0/businesses/<string:id>/reviews/<string:review_id>", methods = ["DELETE"])
+@app.route("/api/v1.0/champions/<string:id>/reviews/<string:review_id>", methods = ["DELETE"])
 @jwt_required
 @admin_required
 def delete_review(id, review_id):
-    #we again update one business using the same ID found in the url
+    #we again update one champion using the same ID found in the url
     #to pull from reviews collection the review with the corresponding ID found in the url
-    businesses.update_one(
+    champions.update_one(
         {"_id" : ObjectId(id)},
         {"$pull" : {"reviews" : { "_id" : ObjectId(review_id) } } }
     )
